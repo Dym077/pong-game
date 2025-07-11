bfdd8ffe2215f91a81b139a012b205282ef470a3
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2025-07-11 17:43:43 +0200

    Setup Screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+screen.exitonclick()
